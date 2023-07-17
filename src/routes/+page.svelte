7899096e0ae74d16f96771ccea5207c437d8615d
LilleAila <olai.solsvik@gmail.com>
2023-07-17 12:39:00 +0200

    Added stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { text } from "@sveltejs/kit";
+
+    // /-\ |3 ( |) |= /= (= |-| | \_| |< |\_ /\\/\ |\\| () |° (). |°\ \_\\- -|- |\_| \\/ \\/\\/ >< \\|/ -/_ /|= (/) /°\
+
+    const ascii: { [key: string]: string } = {
+        A: `/-\\`,
+        B: `|3`,
+        C: `\(`,
+        D: `|)`,
+        E: `|=`,
+        F: `/=`,
+        G: `(=`,
+        H: `|-|`,
+        I: `|`,
+        J: `\_|`,
+        K: `|<`,
+        L: `|\_`,
+        M: `|\\/|`,
+        N: `|\\|`,
+        O: `\()`,
+        P: `|°`,
+        Q: `().`,
+        R: `|°\\`,
+        S: `\_\\-`,
+        T: `-|-`,
+        U: `|\_|`,
+        V: `\\/`,
+        W: `\\/\\/`,
+        X: `><`,
+        Y: `\\|/`,
+        Z: `-/_`,
+        Æ: `/|=`,
+        Ø: `(/)`,
+        Å: `/°\\`
+    }
+    let input = ""
+    $: asciified = input.toUpperCase().replace(new RegExp(Object.keys(ascii).join('|'), 'g'), (match) => ascii[match]);
+    $: asciified_escaped = asciified.replace(/[\\_()*|]/g, '\\$&');
+</script>
+
+<main class="w-full text-center flex justify-center">
+    <div class="flex flex-col justify-center w-4/5 p-8">
+        <h2 class="text-4xl my-2">
+            ASCIIfy text - /-\ _\- ( | |
+        </h2>
+        <input
+            type="text"
+            placeholder="Input"
+            class="input w-full my-2"
+            bind:value={input}
+        />
+        <p class="text-3xl text-primary my-2">{asciified}</p>
+        <p class="text-lg text-secondary my-2">{asciified_escaped}</p>
+    </div>
+</main>
